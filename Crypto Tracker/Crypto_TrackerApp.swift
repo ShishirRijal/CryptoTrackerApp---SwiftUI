c49f65e15b3f8868f29cc1df8413c1da69546282
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-17 02:16:39 +0545

    Initial Commit

---
+++ Crypto Tracker/Crypto_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_TrackerApp.swift
+//  Crypto Tracker
+//
+//  Created by Shishir Rijal on 17/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
