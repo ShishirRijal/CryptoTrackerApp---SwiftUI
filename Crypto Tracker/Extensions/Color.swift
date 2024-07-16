cb7338d31db730d614116a38f4e7490af9b62add
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-17 02:30:40 +0545

    add color extension

---
+++ Crypto Tracker/Extensions/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  Crypto Tracker
+//
+//  Created by Shishir Rijal on 17/07/2024.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let backgroundColor = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondayText = Color("SecondaryTextColor")
+}
