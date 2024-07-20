086166c0c89acf569e24ad9ccb96d6fcc8afd4ec
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-20 22:24:23 +0545

    add coinimage view

--- Crypto Tracker/Core/Components/CoinImage/CoinImageView.swift
+++ Crypto Tracker/Core/Components/CoinImage/CoinImageView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: CoinModel) {
+        _vm =  StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            }
+            else if (vm.isLoading) {
+                ProgressView()
+            }
+            else {
+                Image(systemName: "questionmark")
+                    .foregroundColor(Color.theme.secondayText)
+            }
+        }
     }
 }
 
 #Preview {
-    CoinImageView()
+    CoinImageView(coin: MockCoinModel.coin)
+        .previewLayout(.sizeThatFits)
 }
