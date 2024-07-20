21f9f0db71fdf3fdc936fb6cb624b85a2d0ac629
Shishir Rijal <ccrrizal0892@gmail.com>
2024-07-20 22:24:03 +0545

    add coinimage service

---
+++ Crypto Tracker/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinImageView.swift
+//  Crypto Tracker
+//
+//  Created by Shishir Rijal on 20/07/2024.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinImageView()
+}
---
+++ Crypto Tracker/Core/Components/CoinImage/CoinImageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoinImageViewModel.swift
+//  Crypto Tracker
+//
+//  Created by Shishir Rijal on 20/07/2024.
+//
+
+import Foundation
